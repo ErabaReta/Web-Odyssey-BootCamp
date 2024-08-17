91212c62e151574374eb06ebef118ec348b6b742
El Mokhtar Ouhrich <eouhrich@c3r9p10.1337.ma>
2024-08-17 11:38:07 +0100

    adding animation to movemenets

--- Top-down_2dgame/Player.js
+++ Top-down_2dgame/Player.js
@@ -1,5 +1,5 @@
 export class Player {
-	constructor (name = "adam", characterImage = null, facingDirection = "down", playerX = 0, playerY = 0)
+	constructor (name = "adam", characterImage, facingDirection = "down", playerX = 0, playerY = 0)
 	{
 		this.box = document.createElement("img");
 		this.box.src = characterImage;
@@ -22,23 +22,32 @@ export class Player {
 		this.facingDirection = direction;
 		if (direction == "UP")
 		{
-			if (this.playerY != 0)
-				this.playerY -= 50 ;
+			if (this.playerY >= 0)
+				this.playerY -= 100 ;
+			this.box.src = "./img/gifmaker_me(3).gif"
+			setTimeout(() => {if (this.facingDirection == direction) this.box.src = "./img/gifmaker_me(2).gif"} , 800);
 		}
 		if (direction == "RIGHT")
 		{
-			if (this.playerX != 850)
-				this.playerX += 50 ;
+			if (this.playerX <= 850)
+				this.playerX += 100 ;
+			this.box.src = "./img/gifmaker_me(7).gif"
+			setTimeout(() => {if (this.facingDirection == direction) this.box.src = "./img/gifmaker_me(6).gif"} , 800);
 		}
 		if (direction == "DOWN")
 		{
-			if (this.playerY != 850)
-				this.playerY += 50 ;
+			if (this.playerY <= 850)
+				this.playerY += 100 ;
+			this.box.src = "./img/gifmaker_me(1).gif";
+			setTimeout(() => {if (this.facingDirection == direction) this.box.src = "./img/gifmaker_me.gif"} , 800);
+
 		}
 		if (direction == "LEFT")
 		{
-			if (this.playerX != 0)
-				this.playerX -= 50 ;
+			if (this.playerX >= 0)
+				this.playerX -= 100 ;
+			this.box.src = "./img/gifmaker_me(5).gif"
+			setTimeout(() => {if (this.facingDirection == direction){this.box.src = "./img/gifmaker_me(4).gif"}} , 800);
 		}
 		this.box.style.top = this.playerY + "px";
 		this.box.style.left = this.playerX + "px";
--- Top-down_2dgame/script.js
+++ Top-down_2dgame/script.js
@@ -1,11 +1,11 @@
 import { Player } from "./Player.js";
 const map = document.getElementById("map");
 
-const player = new Player("steve", "img/Warrior_animations/Front/PNG Sequences/Warrior_clothes_1/Idle/0_Warrior_Idle_000.png", );
+const player = new Player("steve", "./img/gifmaker_me.gif");
 
 player.spawn(500 , 500, map);
 
-document.onkeydown = (e) => {
+document.onkeyup = (e) => {
 	if (e.keyCode === 38) {
 		player.move("UP");
 	//   console.log('up arrow pressed');
